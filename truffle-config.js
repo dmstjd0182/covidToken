fb6767d69afd61c8a1fc69e6a13b16b1eff6f6c2
dmstjd0182 <dmstjd0182@snu.ac.kr>
2021-11-26 21:01:49 +0900

    프론트엔드 구조 공사중

--- truffle-config.js
+++ truffle-config.js
@@ -24,7 +24,7 @@
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
-  contracts_build_directory: "./app/src/build/contracts",
+  contracts_build_directory: "./client/src/build/contracts",
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
