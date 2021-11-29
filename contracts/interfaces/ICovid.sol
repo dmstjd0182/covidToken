1b6c3a3901e91d208faabe3b1b207f26eea1b1b3
dmstjd0182 <dmstjd0182@snu.ac.kr>
2021-11-30 02:00:05 +0900

    총 전염 스코어 추가

--- contracts/interfaces/ICovid.sol
+++ contracts/interfaces/ICovid.sol
@@ -15,6 +15,7 @@ interface ICovid{
     function initialSupply() external pure returns (uint256);
     function totalSupply() external view returns (uint256);
     function rewardPool() external view returns (uint256);
+    function totalInfectingOrder() external view returns (uint256);
 
     function infected(uint256 id) external view returns (address);
     function userInfo(address user) external view returns (uint256 lastBalance, uint256 time, uint256 infectingCount, uint256 infectingOrder, bool isInfected, bool canClaimReward, address infectedFrom);
